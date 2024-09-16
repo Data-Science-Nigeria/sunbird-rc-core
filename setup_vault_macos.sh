4098c3eba30036a32eb66486f22bab8adad0a5dc
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2024-09-16 09:26:34 +0000

    fix: Refactored the schemas to conform with the required contexts

--- setup_vault_macos.sh
+++ setup_vault_macos.sh
@@ -15,11 +15,11 @@ SERVICE_NAME="${2:-vault}"
 
 echo "Setting up $SERVICE_NAME in $COMPOSE_FILE"
 
-docker-compose -f "$COMPOSE_FILE" up -d "$SERVICE_NAME"
+docker compose -f "$COMPOSE_FILE" up -d "$SERVICE_NAME"
 
 # Function to check if Vault is ready
 check_vault_status() {
-  vault_status=$(docker-compose -f "$COMPOSE_FILE" exec -T "$SERVICE_NAME" vault status 2>&1)
+  vault_status=$(docker compose -f "$COMPOSE_FILE" exec -T "$SERVICE_NAME" vault status 2>&1)
   if [[ $vault_status == *"connection refused"* ]]; then
     echo "Unable to connect to Vault. Waiting for Vault to start..."
     return 1
@@ -43,14 +43,14 @@ if [[ $vault_status == *"Initialized             true"* ]]; then
     echo "Vault is already initialized. Unsealing if necessary..."
 else
   # Initialize Vault and save keys to files
-  docker-compose -f "$COMPOSE_FILE" exec -T "$SERVICE_NAME" vault operator init > ansi-keys.txt
+  docker compose -f "$COMPOSE_FILE" exec -T "$SERVICE_NAME" vault operator init > ansi-keys.txt
   sed $'s/\x1B\[[0-9;]*[JKmsu]//g' ansi-keys.txt > keys.txt
 fi
 
 # Unseal Vault
 for i in {1..3}; do
   key=$(sed -n "s/Unseal Key $i: \(.*\)/\1/p" keys.txt)
-  docker-compose -f "$COMPOSE_FILE" exec -T "$SERVICE_NAME" vault operator unseal "$key" < /dev/null
+  docker compose -f "$COMPOSE_FILE" exec -T "$SERVICE_NAME" vault operator unseal "$key" < /dev/null
 done
 
 # Get the root token
@@ -65,7 +65,7 @@ fi
 
 # Enable KV v2 secrets engine if Vault was just initialized
 if [[ $vault_status != *"Initialized             true"* ]]; then
-  docker-compose -f "$COMPOSE_FILE" exec -T -e VAULT_TOKEN="$root_token" "$SERVICE_NAME" vault secrets enable -path=kv kv-v2
+  docker compose -f "$COMPOSE_FILE" exec -T -e VAULT_TOKEN="$root_token" "$SERVICE_NAME" vault secrets enable -path=kv kv-v2
 fi
 
 echo -e "\nNOTE: KEYS ARE STORED IN keys.txt"
